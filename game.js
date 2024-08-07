93f2e01b910bc78144dabd02efe9536878b106a1
Esther <elvynnn@gmail.com>
2024-08-08 07:53:17 +0900

    Dead

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
 /* global Phaser */
 
+import { createAnimations } from './assets/animations.js';
+
 const config = {
   type: Phaser.AUTO,
   width: 256,
   height: 244,
-  backgroundColor: "#049cd8",
-  parent: "game",
+  backgroundColor: '#049cd8',
+  parent: 'game',
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 500 },
       debug: false,
     },
   },
@@ -23,73 +25,82 @@ const config = {
 new Phaser.Game(config);
 
 function preload() {
-  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
-  this.load.spritesheet("mario", "assets/entities/mario.png", {
+  this.load.image('cloud1', 'assets/scenery/overworld/cloud1.png');
+  this.load.spritesheet('mario', 'assets/entities/mario.png', {
     frameWidth: 18,
     frameHeight: 16,
   });
-  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
+  this.load.image('floorbricks', 'assets/scenery/overworld/floorbricks.png');
+  this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
 }
 
 function create() {
-  this.add.image(150, 50, "cloud1").setScale(0.15).setOrigin(0, 0); //para que tome como referencia la esquina izquierda superior y no el centro de la imagen
+  this.add.image(150, 50, 'cloud1').setScale(0.15).setOrigin(0, 0); //para que tome como referencia la esquina izquierda superior y no el centro de la imagen
 
   //suelo
   this.floor = this.physics.add.staticGroup();
 
   this.floor
-    .create(0, config.height - 16, "floorbricks")
+    .create(0, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
     .refreshBody();
 
   this.floor
-    .create(150, config.height - 16, "floorbricks")
+    .create(150, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
     .refreshBody();
-     
 
   this.mario = this.physics.add
-    .sprite(50, 100, "mario")
+    .sprite(50, 100, 'mario')
     .setOrigin(0, 1)
-    .setCollideWorldBounds(true);
+    .setCollideWorldBounds(true)
+    .setGravityY(300);
 
- //Colisiones
- this.physics.add.collider(this.mario, this.floor);
+  //Tamaño del mundo
+  this.physics.world.setBounds(0, 0, 2000, config.height);
 
-  this.anims.create({
-    key: "mario-walk",
-    frames: this.anims.generateFrameNumbers("mario", { start: 3, end: 1 }),
-    frameRate: 12,
-    repeat: -1,
-  });
+  //Colisiones
+  this.physics.add.collider(this.mario, this.floor);
 
-  this.anims.create({
-    key: "mario-idle",
-    frames: [{ key: "mario", frame: 0 }],
-  });
+  //Camara
+  this.cameras.main.setBounds(0, 0, 2000, config.height);
+  this.cameras.main.startFollow(this.mario);
 
-  this.anims.create({
-    key: "mario-jump",
-    frames: [{ key: "mario", frame: 5 }],
-  });
+  createAnimations(this);
 
   this.keys = this.input.keyboard.createCursorKeys();
 }
 function update() {
+  if (this.mario.isDead) return;
   if (this.keys.left.isDown) {
-    this.mario.anims.play("mario-walk", true);
+    this.mario.anims.play('mario-walk', true);
     this.mario.x -= 2;
     this.mario.flipX = true;
   } else if (this.keys.right.isDown) {
-    this.mario.anims.play("mario-walk", true);
+    this.mario.anims.play('mario-walk', true);
     this.mario.x += 2;
     this.mario.flipX = false;
   } else {
-    this.mario.anims.play("mario-idle", true);
+    this.mario.anims.play('mario-idle', true);
   }
 
-  if (this.keys.up.isDown ) {
-    this.mario.y -= 5;
-    this.mario.anims.play("mario-jump", true);
+  if (this.keys.up.isDown && this.mario.body.touching.down) {
+    this.mario.setVelocityY(-300);
+    this.mario.anims.play('mario-jump', true);
+  }
+
+  if (this.mario.y >= config.height) {
+    this.mario.isDead = true;
+    this.mario.anims.play('mario-dead');
+    this.mario.setCollideWorldBounds(false);
+    this.sound.add('gameover', {volume: 0.4}).play()
+
+    setTimeout(() => {
+      this.mario.setVelocityY(-350);
+    }, 100);
+
+    setTimeout(() => {
+      this.scene.restart();
+    }, 2000);
   }
 }
