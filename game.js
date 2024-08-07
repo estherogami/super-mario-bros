57b441f03c3d64b1e03009521a33f46b7d887126
Esther <elvynnn@gmail.com>
2024-08-08 05:43:05 +0900

    Added left right movement on mario

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const config = {
     type: Phaser.AUTO,
     width: 256,
     height: 244,
-    backgroundColor: '#fff',
+    backgroundColor: '#049cd8',
     parent: 'game',
     scene: {
         preload, //precargar recursos
@@ -16,12 +16,71 @@ const config = {
 new Phaser.Game(config);
 
 function preload () {
-    console.log("preload")
+    this.load.image(
+        'cloud1',
+        'assets/scenery/overworld/cloud1.png'
+    )
+    this.load.spritesheet(
+        'mario',
+        'assets/entities/mario.png',
+        { frameWidth: 18, frameHeight: 16}
+    )
+    this.load.image(
+        'floorbricks',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+
+    
 }
 
 function create () {
-    console.log("create")
+    this.add.image(150,50, 'cloud1')
+    .setScale(0.15)
+    .setOrigin(0, 0) //para que tome como referencia la esquina izquierda superior y no el centro de la imagen
+
+    this.add.tileSprite(0, config.height - 32, config.width, 32, 'floorbricks')
+    .setOrigin(0,0)
+
+   this.mario = this.add.sprite(50, 210 , 'mario')
+    .setOrigin(0, 1)
+
+    this.anims.create({
+        key: 'mario-walk',
+        frames: this.anims.generateFrameNumbers(
+            'mario',
+            { start: 3, end: 1}
+        ),
+        frameRate: 12,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'mario-idle',
+        frames: [{key: 'mario', frame: 0}]
+    })
+
+    this.anims.create({
+        key: 'mario-jump',
+        frames: [{key: 'mario', frame: 5}]
+    })
+    
+    this.keys = this.input.keyboard.createCursorKeys()
 }
  function update () {
-    // console.log("update")
+    if (this.keys.left.isDown) {
+        this.mario.anims.play('mario-walk', true)
+        this.mario.x -= 2
+        this.mario.flipX = true
+    } else if (this.keys.right.isDown) {
+        this.mario.anims.play('mario-walk', true)
+        this.mario.x += 2
+        this.mario.flipX = false
+    } else {
+        this.mario.anims.play('mario-idle', true)
+    }
+
+    if (this.keys.up.isDown){
+        this.mario.y -= 5 
+        this.mario.anims.play('mario-jump', true)
+    }
  }
