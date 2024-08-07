6998eb4001bc6ef03ff57e9e21ef6c28891962f5
Esther <elvynnn@gmail.com>
2024-08-08 04:36:40 +0900

    Inicializamos juego

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 244,
+    backgroundColor: '#fff',
+    parent: 'game',
+    scene: {
+        preload, //precargar recursos
+        create, //se ejecuta cuando el juego comienza
+        update //se ejecuta en cada frame
+    }
+}
+
+new Phaser.Game(config);
+
+function preload () {
+    console.log("preload")
+}
+
+function create () {
+    console.log("create")
+}
+ function update () {
+    // console.log("update")
+ }
