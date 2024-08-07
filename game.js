141949e086fb8f130b9cdac30257f8643987fee4
Esther <elvynnn@gmail.com>
2024-08-08 06:22:18 +0900

    Added physics and colisions to mario

--- game.js
+++ game.js
@@ -1,86 +1,95 @@
 /* global Phaser */
 
 const config = {
-    type: Phaser.AUTO,
-    width: 256,
-    height: 244,
-    backgroundColor: '#049cd8',
-    parent: 'game',
-    scene: {
-        preload, //precargar recursos
-        create, //se ejecuta cuando el juego comienza
-        update //se ejecuta en cada frame
-    }
-}
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: "#049cd8",
+  parent: "game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload, //precargar recursos
+    create, //se ejecuta cuando el juego comienza
+    update, //se ejecuta en cada frame
+  },
+};
 
 new Phaser.Game(config);
 
-function preload () {
-    this.load.image(
-        'cloud1',
-        'assets/scenery/overworld/cloud1.png'
-    )
-    this.load.spritesheet(
-        'mario',
-        'assets/entities/mario.png',
-        { frameWidth: 18, frameHeight: 16}
-    )
-    this.load.image(
-        'floorbricks',
-        'assets/scenery/overworld/floorbricks.png'
-    )
-
-    
+function preload() {
+  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
+  this.load.spritesheet("mario", "assets/entities/mario.png", {
+    frameWidth: 18,
+    frameHeight: 16,
+  });
+  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
 }
 
-function create () {
-    this.add.image(150,50, 'cloud1')
-    .setScale(0.15)
-    .setOrigin(0, 0) //para que tome como referencia la esquina izquierda superior y no el centro de la imagen
+function create() {
+  this.add.image(150, 50, "cloud1").setScale(0.15).setOrigin(0, 0); //para que tome como referencia la esquina izquierda superior y no el centro de la imagen
 
-    this.add.tileSprite(0, config.height - 32, config.width, 32, 'floorbricks')
-    .setOrigin(0,0)
+  //suelo
+  this.floor = this.physics.add.staticGroup();
 
-   this.mario = this.add.sprite(50, 210 , 'mario')
+  this.floor
+    .create(0, config.height - 16, "floorbricks")
+    .setOrigin(0, 0.5)
+    .refreshBody();
+
+  this.floor
+    .create(150, config.height - 16, "floorbricks")
+    .setOrigin(0, 0.5)
+    .refreshBody();
+     
+
+  this.mario = this.physics.add
+    .sprite(50, 100, "mario")
     .setOrigin(0, 1)
+    .setCollideWorldBounds(true);
 
-    this.anims.create({
-        key: 'mario-walk',
-        frames: this.anims.generateFrameNumbers(
-            'mario',
-            { start: 3, end: 1}
-        ),
-        frameRate: 12,
-        repeat: -1
-    })
+ //Colisiones
+ this.physics.add.collider(this.mario, this.floor);
 
-    this.anims.create({
-        key: 'mario-idle',
-        frames: [{key: 'mario', frame: 0}]
-    })
+  this.anims.create({
+    key: "mario-walk",
+    frames: this.anims.generateFrameNumbers("mario", { start: 3, end: 1 }),
+    frameRate: 12,
+    repeat: -1,
+  });
 
-    this.anims.create({
-        key: 'mario-jump',
-        frames: [{key: 'mario', frame: 5}]
-    })
-    
-    this.keys = this.input.keyboard.createCursorKeys()
+  this.anims.create({
+    key: "mario-idle",
+    frames: [{ key: "mario", frame: 0 }],
+  });
+
+  this.anims.create({
+    key: "mario-jump",
+    frames: [{ key: "mario", frame: 5 }],
+  });
+
+  this.keys = this.input.keyboard.createCursorKeys();
 }
- function update () {
-    if (this.keys.left.isDown) {
-        this.mario.anims.play('mario-walk', true)
-        this.mario.x -= 2
-        this.mario.flipX = true
-    } else if (this.keys.right.isDown) {
-        this.mario.anims.play('mario-walk', true)
-        this.mario.x += 2
-        this.mario.flipX = false
-    } else {
-        this.mario.anims.play('mario-idle', true)
-    }
+function update() {
+  if (this.keys.left.isDown) {
+    this.mario.anims.play("mario-walk", true);
+    this.mario.x -= 2;
+    this.mario.flipX = true;
+  } else if (this.keys.right.isDown) {
+    this.mario.anims.play("mario-walk", true);
+    this.mario.x += 2;
+    this.mario.flipX = false;
+  } else {
+    this.mario.anims.play("mario-idle", true);
+  }
 
-    if (this.keys.up.isDown){
-        this.mario.y -= 5 
-        this.mario.anims.play('mario-jump', true)
-    }
- }
+  if (this.keys.up.isDown ) {
+    this.mario.y -= 5;
+    this.mario.anims.play("mario-jump", true);
+  }
+}
